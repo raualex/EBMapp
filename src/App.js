82e31b5b3aade108ec8383383f18d33113159bcd
Alexander Rau <alexanderrau86@yahoo.com>
2018-09-20 15:19:01 -0600

    Add title and build test container component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './CSS/App.css';
 import Header from './Header.js'
+import BandInfo from './BandInfo.js'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      entered: false
+    }
+
+    this.takeEnterInfo = this.takeEnterInfo.bind(this)
+  }
+
+  takeEnterInfo(enterInfo) {
+    this.setState({
+      entered: true
+    })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
+    if (this.state.entered === false) {
+      return (
+        <div className="App">
+          <Header takeEnterInfo={this.takeEnterInfo}/>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <BandInfo />
+        </div>
+      );      
+    }
   }
 }
 
---
+++ src/BandInfo.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './CSS/BandInfo.css'
+
+
+class BandInfo extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div className="info-container">
+        <p>TEST</p>
+      </div>
+    )
+  }
+
+
+
+
+
+
+}
+
+
+export default BandInfo;
