a20ef1f34a0121d444096b6879b9d5a27e715b87
Alexander Rau <alexanderrau86@yahoo.com>
2018-09-20 12:32:56 -0600

    Add hover effects

--- src/Header.js
+++ src/Header.js
@@ -6,13 +6,27 @@ import './CSS/Header.css'
 class Header extends Component {
   constructor() {
     super();
+
+    this.state = {
+      isHovered: false
+    }
+
+    this.handleHover = this.handleHover.bind(this)
+  }
+
+  handleHover() {
+    this.setState({
+      isHovered: !this.state.isHovered
+    })
   }
 
   render() {
+    const imgClass = this.state.isHovered ? "Faster-spin" : "App-logo"
+
     return(
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="Enter-btn">ENTER</button>
+        <img src={logo} className={imgClass} alt="logo" />
+        <button className="Enter-btn" onMouseEnter={this.handleHover}  onMouseLeave={this.handleHover}>ENTER</button>
       </header>
     )
   }
