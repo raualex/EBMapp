1c95a8468357641b1e8b97a2d5012c82eaf6b9a6
Alexander Rau <alexanderrau86@yahoo.com>
2018-09-21 11:41:31 -0600

    Add Card files

--- src/BandInfo.js
+++ src/BandInfo.js
@@ -5,16 +5,27 @@ import './CSS/BandInfo.css'
 class BandInfo extends Component {
   constructor() {
     super();
+
+    this.state = {
+      cardName: ""
+    }
+
+    this.printCard = this.printCard.bind(this)
+  }
+
+  printCard(event) {
+    this.setState({
+      cardName: event.target.innerHTML
+    })
   }
 
   render() {
-    console.log(this.props)
     let bandNames = Object.keys(this.props.bandData)
-    console.log(bandNames)
+
     return bandNames.map((name, index) => {
       return(
         <div className="info-container" key={index}>
-          <h3 className="bandLink">{this.props.bandData[name].bandName}</h3>
+          <ul className="bandLink" onClick={this.printCard}>{this.props.bandData[name].bandName}</ul>
         </div>
       )
     })
