12b50cc08dff13c51fd1745c79aac395931aa013
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-07 14:49:38 -0700

    Successfully build schemas and seed database - both tables

--- db/migrations/20190206114801_initial.js
+++ db/migrations/20190206114801_initial.js
@@ -1,25 +1,33 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('ebm-bands', function(table) {
+    knex.schema.createTable('ebm_bands', function(table) {
       table.increments('id').primary();
-      table.string('bandName');
-      table.string('currentMembers');
-      table.string('pastMembers');
-      table.boolean('stillActive');
-      table.string('countryOfOrigin');
+      table.string('band_name');
+      table.string('current_members');
+      table.string('past_members');
+      table.boolean('still_active');
+      table.string('country_of_origin');
 
       table.timestamps(true, true);
     }),
 
     knex.schema.createTable('albums', function(table) {
       table.increments('id').primary();
-      table.string('albumTitle');
-      
+      table.string('album_title');
+      table.string('album_type');
+      table.integer('band_id').unsigned()
+      table.foreign('band_id')
+        .references('ebm_bands.id');
+
+      table.timestamps(true, true);
     })
   ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('albums'),
+    knex.schema.dropTable('ebm_bands')
+  ]);
 };
