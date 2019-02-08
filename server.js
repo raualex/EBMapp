d4bfb66af8a6de879a1619a231d2dd60bd6dc695
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-07 19:42:56 -0700

    Finish initial server build and start adding in thunks and reducers

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const environment = process.env.NODE_ENV || 'development'
-// const config = require('./knexfile')[environment]
-// const database = require('knex')(config)
-// const app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const database = require('knex')(config)
+const app = express()
 
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'EBM Bands';
+app.use(express.static('public'));
+
+app.get('/api/v1/ebm_bands', (request, response) => {
+  database('ebm_bands').select()
+    .then((bands) => {
+      response.status(200).json(bands);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
