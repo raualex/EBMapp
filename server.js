9934931639bd250a4478d5084a75e1be92c18687
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-15 13:08:13 -0700

    Add cors fixes to server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+var cors = require('cors');
 const bodyParser = require('body-parser')
 const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const database = require('knex')(config)
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'EBM Bands';
