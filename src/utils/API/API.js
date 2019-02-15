019550f8ae58155fe93dc58b5b8965903a17440e
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-15 12:57:04 -0700

    Attempt to fix server CORS issue

--- src/utils/API/API.js
+++ src/utils/API/API.js
@@ -1,5 +1,5 @@
 export const retrieveBands = async () => {
-  const response = await fetch('/api/v1/ebm_bands')
+  const response = await fetch('https://ebm-app.herokuapp.com/api/v1/ebm_bands')
   console.log(response)
   const data = await response.text()
   return data
