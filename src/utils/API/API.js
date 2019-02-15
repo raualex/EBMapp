4b8e28921349c35690909541eeea36fd9ceaf070
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-15 13:18:16 -0700

    Fix fetch call parse

--- src/utils/API/API.js
+++ src/utils/API/API.js
@@ -1,6 +1,6 @@
 export const retrieveBands = async () => {
   const response = await fetch('https://ebm-app.herokuapp.com/api/v1/ebm_bands')
   console.log(response)
-  const data = await response.text()
+  const data = await response.json()
   return data
 }
