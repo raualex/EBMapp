8f43423da12b1c07edfc2bf752920430026fc077
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-16 13:28:56 -0700

    Add endpoint to GET all albums

--- server.js
+++ server.js
@@ -22,12 +22,22 @@ app.get('/api/v1/ebm_bands', (request, response) => {
     });
 });
 
+app.get('/api/v1/albums', (request, response) => {
+  database('albums').select()
+    .then((albums) => {
+      response.status(200).json(albums);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 app.get('/api/v1/albums/:band_id', (request, response) => {
   const { band_id } = request.params
   database('albums').where('band_id', band_id).select()
     .then(albums => response.status(200).json(albums))
-    .catch(error => response.status(500).json(`Error fetching albums: ${error.message}`))
-})
+    .catch(error => response.status(500).json(`Error fetching albums: ${error.message}`));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
