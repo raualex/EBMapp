bc5c8a6b9c42d4532f616eac37fcff8a341b6047
Alexander Rau <alexanderrau86@yahoo.com>
2019-03-22 17:17:32 -0600

    Add data attributes to Li elements and have the delete button get that info

--- src/components/MemberCard/index.js
+++ src/components/MemberCard/index.js
@@ -4,10 +4,24 @@ import uuid from 'uuid';
 
 class MemberCard extends Component {
 
+  handleClick = (event) => {
+    event.preventDefault()
+    console.log(event.target.parentNode.dataset.id)
+  }
+
   render() {
     let { memberNames } = this.props
     let membersList = memberNames.map((name) => {
-      return <li key={uuid()} className='member-name'>{name}</li>
+      return <li 
+               key={uuid()} 
+               className='member-name'
+               data-id={name}
+             >{name}
+               <button 
+                 key={uuid()}
+                 onClick={this.handleClick}
+               >X</button>
+             </li>
     });
 
     return (
